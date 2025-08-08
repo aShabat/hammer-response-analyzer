a7076b5213239bbfc8c9794b8e19acd5b9f93fa3
Anton Shabat <anton.shabat@gmail.com>
2025-08-08 17:01:40 +0300

    added option for spectrum input

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import numpy as np
 from RFP import rfp_poles
 
 # Parameters (better add way for user to set them)
-responses_dir = "RESPONSES"
-parameters_dir = os.path.curdir
+responses_dir = os.path.join(os.path.dirname(__file__), "RESPONSES")
+parameters_dir = os.path.dirname(__file__)
+data_type = "spectrum"  # can change to "signal"
+reference_acceleration = (
+    True  # needed only for data_type = signal. Is reference measured as acceleration?
+)
 reference_channel = 2
 sampling_freq = 100000
 sampling_time = 0.1
@@ -15,44 +19,79 @@ df = 1 / sampling_time
 response_length = int(sampling_time * sampling_freq)
 
 # Loading responses
-file_names = os.listdir(responses_dir)
-strings, channels = [], []
-for name in file_names:
-    match = re.match(r"^(\d+)_(\d+).txt$", name)
-    if match == None:
-        continue
-    string, channel = map(int, match.groups())
-    if string not in strings:
-        strings.append(string)
-    if channel not in channels:
-        channels.append(channel)
-strings.sort()
-channels.sort()
-reference_channel_index = channels.index(reference_channel)
+if data_type == "signal":
+    file_names = os.listdir(responses_dir)
+    strings, channels = [], []
+    for name in file_names:
+        match = re.match(r"^(\d+)_(\d+).txt$", name)
+        if match == None:
+            continue
+        string, channel = map(int, match.groups())
+        if string not in strings:
+            strings.append(string)
+        if channel not in channels:
+            channels.append(channel)
+    strings.sort()
+    channels.sort()
+    reference_channel_index = channels.index(reference_channel)
 
-responses = [[[]] * len(channels) for _ in strings]
-for string in range(len(strings)):
-    for channel in range(len(channels)):
-        file_path = os.path.join(
-            responses_dir, f"{strings[string]}_{channels[channel]}.txt"
-        )
-        response = [float(line) for line in open(file_path)]
-        if len(response) > response_length:
-            response = response[:response_length]
-        else:
-            response = response + [0] * (response_length - len(response))
-        responses[string][channel] = response
-responses = np.array(responses, dtype=np.cdouble)
-references = responses[:, reference_channel_index]
-responses = np.delete(responses, reference_channel_index, 1)
+    responses = [[[]] * len(channels) for _ in strings]
+    for string in range(len(strings)):
+        for channel in range(len(channels)):
+            file_path = os.path.join(
+                responses_dir, f"{strings[string]}_{channels[channel]}.txt"
+            )
+            response = [float(line) for line in open(file_path)]
+            if len(response) > response_length:
+                response = response[:response_length]
+            else:
+                response = response + [0] * (response_length - len(response))
+            responses[string][channel] = response
+    responses = np.array(responses, dtype=np.cdouble)
+    references = responses[:, reference_channel_index]
+    responses = np.delete(responses, reference_channel_index, 1)
 
-# Apply windows???
+    # Apply windows???
 
-Hs = (
-    np.arange(0, df * references.shape[-1], df)
-    * np.fft.fft(responses)
-    / np.max(np.fft.fft(references)[:, np.newaxis], 0)
-)
+    frequencies = np.arange(0, response_length, df)
+
+    Hs = np.fft.fft(responses) / np.max(np.fft.fft(references)[:, np.newaxis], 0)
+    if reference_acceleration:
+        Hs *= 2 * np.pi * frequencies
+elif data_type == "spectrum":
+    file_names = os.listdir(responses_dir)
+    strings, channels = [], []
+    for name in file_names:
+        match = re.match(r"^(\d+)_(\d+)_abs.txt$", name)
+        if match == None:
+            continue
+        string, channel = map(int, match.groups())
+        if string not in strings:
+            strings.append(string)
+        if channel not in channels:
+            channels.append(channel)
+    strings.sort()
+    channels.sort()
+    reference_channel_index = channels.index(reference_channel)
+
+    frequencies = np.array(
+        [float(line) for line in open(os.path.join(responses_dir, "freq.txt"))]
+    )
+    Hs = np.zeros((len(strings), len(channels), len(frequencies)))
+    for string in range(len(strings)):
+        for channel in range(len(channels)):
+            abs_path = os.path.join(
+                responses_dir, f"{strings[string]}_{channels[channel]}_abs.txt"
+            )
+            phase_path = os.path.join(
+                responses_dir, f"{strings[string]}_{channels[channel]}_phase.txt"
+            )
+            abs = np.array([float(line) for line in open(abs_path)])
+            phase = np.array([float(line) for line in open(phase_path)])
+            Hs[string, channel] = abs * np.exp(1j * np.pi * phase)
+else:
+    raise Exception("choose a data_type")
+
 borders = [
     30,
     500,
@@ -67,14 +106,15 @@ borders = [
 poles = None
 for i in range(len(borders) - 1):
     new_poles = []
-    omega = 2 * np.pi * np.arange(borders[i], borders[i + 1], df)
+    interval = frequencies < borders[i + 1] and frequencies >= borders[i]
+    omega = 2 * np.pi * frequencies[interval]
     for n in range(30, -2, -2):
         if n == 0:
             new_poles = []
             break
         new_poles = rfp_poles(
             omega,
-            Hs[:, :, int(borders[i] // df) : int(borders[i + 1] // df)],
+            Hs[:, :, interval],
             30,
             n,
         )
@@ -94,8 +134,8 @@ poles = poles[  # pyright: ignore[reportOptionalSubscript]
 ]
 
 pole_freqs = np.imag(poles) / (2 * np.pi)
-channel_amplitudes = np.zeros((0, 6))
-strings_amplitudes = np.zeros((0, 64))
+channels_amplitudes = np.zeros((0, len(channels)))
+strings_amplitudes = np.zeros((0, len(strings)))
 masses = np.array([])
 
 
@@ -116,7 +156,7 @@ for pole in poles:
     new_channel_amplitudes /= np.max(np.abs(new_channel_amplitudes))
     new_strings_amplitudes /= np.max(np.abs(new_strings_amplitudes))
     m = 1 / (2 * q * np.imag(pole))
-    channel_amplitudes = np.vstack((channel_amplitudes, new_channel_amplitudes))
+    channels_amplitudes = np.vstack((channels_amplitudes, new_channel_amplitudes))
     strings_amplitudes = np.vstack((strings_amplitudes, new_strings_amplitudes))
     masses = np.append(masses, m)
 
@@ -130,7 +170,7 @@ for row, channel in enumerate(channels):
         continue
     if row > 2:
         row -= 1
-    file_channel_amplitudes[channel, : len(poles)] = channel_amplitudes[:, row]
+    file_channel_amplitudes[channel, : len(poles)] = channels_amplitudes[:, row]
 
 for row, string in enumerate(strings):
     if string % 2 == 0:
